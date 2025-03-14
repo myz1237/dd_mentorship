d9b6058c68285b0f116574615d89780d31534bfd
myz1237 <mengyizhuoprivate@outlook.com>
2025-03-14 20:08:41 +0800

    fix: pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -7,7 +7,7 @@ module.exports = {
 			autorestart: true,
 			exec_mode: 'fork',
 			watch: true,
-			script: './dist/index.js',
+			script: 'pnpm start',
 			error_file: `~/.pm2/logs/${projectName}-error.log`,
 			out_file: `~/.pm2/logs/${projectName}-out.log`,
 			ignore_watch: ['node_modules', 'src'],
